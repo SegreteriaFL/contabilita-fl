aa5f9fefbfcbb5923db24bf28bf1ba384844947c
SegreteriaFL <segreteria@fedeeluce.it>
2025-04-01 11:21:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ elif sezione_attiva == "Rendiconto ETS":
         st.markdown("### Sezione B - Uscite")
         st.dataframe(sezione_b)
 
-        testo_pdf = "Rendiconto ETS\n\nEntrate:\n"
+        testo_pdf = "Rendiconto ETS
 
 Entrate:
 "
