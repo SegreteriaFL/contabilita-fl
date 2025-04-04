6b9f0486f8531628e85717091b7eaa43bc35e48b
SegreteriaFL <segreteria@fedeeluce.it>
2025-04-04 02:23:44 +0200

    Add files via upload

--- sheets_utils.py
+++ sheets_utils.py
@@ -1,3 +1,4 @@
+
 import gspread
 import json
 import os
@@ -5,7 +6,7 @@ from google.oauth2.service_account import Credentials
 import pandas as pd
 import streamlit as st
 
-SHEET_NAME = "prima_nota_2024.xls"  # Nome esatto del file Google Sheet
+SHEET_NAME = "prima_nota_2024.xls"
 
 def get_gsheet_client():
     try:
